850aabaebd72aec093e57ff952a1a1850969ea7a
Ivan Khomyakov <ivan.khomyakov@scale.com>
2022-03-18 17:05:01 -0400

    ignore mypy error

--- launch/logger.py
+++ launch/logger.py
@@ -1,9 +1,15 @@
 import logging
+import typing
 
 import requests
 
 logger = logging.getLogger(__name__)
 logging.basicConfig()
-logging.getLogger(
-    requests.packages.urllib3.__package__  # pylint: disable=no-member
-).setLevel(logging.ERROR)
+
+if typing.TYPE_CHECKING:
+    # Ignore the following code because `requests.packages` does not pass mypy
+    pass
+else:
+    logging.getLogger(
+        requests.packages.urllib3.__package__  # pylint: disable=no-member
+    ).setLevel(logging.ERROR)
