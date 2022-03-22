e85230b137b1a90de30ddaee644366f96ed30f08
Sean Shi <sean.shi@scale.com>
2022-03-22 12:40:43 -0700

    pythonic

--- launch/utils.py
+++ launch/utils.py
@@ -5,8 +5,5 @@ def trim_kwargs(kwargs_dict: Dict[Any, Any]):
     """
     Returns a copy of kwargs_dict with None values removed
     """
-    dict_copy = {}
-    for k, v in kwargs_dict.items():
-        if v is not None:
-            dict_copy[k] = v
+    dict_copy = {k: v for k, v in kwargs_dict.items() if v is not None}
     return dict_copy
