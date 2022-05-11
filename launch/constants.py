76773c7ba11bbee875d1f699984d19f9db599103
Sean Shi <sean.shi@scale.com>
2022-05-10 18:47:27 -0700

    typo

--- launch/constants.py
+++ launch/constants.py
@@ -4,7 +4,7 @@ ASYNC_TASK_PATH = "task_async"
 ASYNC_TASK_RESULT_PATH = "task/result"
 SYNC_TASK_PATH = "task_sync"
 BATCH_TASK_PATH = "batch_task_async"
-BATCH_TASK_RESULTS_PATH = "batch_task_async_result"
+BATCH_TASK_RESULTS_PATH = "batch_task_async/result"
 SCALE_LAUNCH_ENDPOINT = "https://api.scale.com/v1/hosted_inference"
 
 DEFAULT_NETWORK_TIMEOUT_SEC = 120
