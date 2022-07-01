ee02c931c9f645c09c4e789e9a7b034d2d513981
Tianwei She <tianwei.she@scale.com>
2022-07-01 21:28:48 +0800

    try self-hosted internal client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
 import io
+import logging
 import os
 import shutil
 import tempfile
@@ -14,6 +15,9 @@ from boto3 import Session
 import launch
 from launch.errors import APIError
 from launch.model_endpoint import EndpointRequest
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def _upload_bundle_fn(serialized_bundle, bundle_url):
@@ -39,15 +43,8 @@ def _bundle_location_fn():
 
 
 @pytest.fixture()
-def mock_client():
+def launch_client():
     # TODO: change API_KEY
-    # tianwei_api_key = (
-    #     "scaleint_b03c26486af74848a237e0df9f6971b0|61a67d767bce560024c7eb96"
-    # )
-    # # client = launch.LaunchClient(api_key="test")
-    # client = launch.LaunchClient(api_key=tianwei_api_key)
-
-    # copied from hosted_model_inference/tests/integration/endpoint_builder/client_e2e.py
     internal_endpoint = "http://hostedinference.ml-staging-internal.scale.com"
     tianwei_user = "61a67d767bce560024c7eb96"
     # use self_hosted version so that the client can access s3 scale-ml bucket
@@ -63,6 +60,8 @@ def mock_client():
 @pytest.fixture()
 def fake_project_dir():
     tmpdir = tempfile.mkdtemp()
+    logger.info(f"tmpdir: {tmpdir}")
+
     try:
         os.mkdir(os.path.join(tmpdir, "project_root"))
         os.mkdir(os.path.join(tmpdir, "project_root", "my_module1"))
@@ -86,54 +85,34 @@ def fake_project_dir():
         shutil.rmtree(tmpdir)
 
 
-# def test_create_model_bundle_from_dirs_bundle_contents_correct(
-#     mock_client, requests_mock, fake_project_dir  # noqa: F811
-# ):
-#     def check_bundle_upload_data(request):
-#         request_body = request._request.body
-#         zf = ZipFile(io.BytesIO(request_body), "r")
-#         try:
-#             actual_zip_filenames = set(zf.namelist())
-#             assert actual_zip_filenames == set(
-#                 ["my_module1/requirements.txt", "my_module2/foobar.txt"]
-#             )
-#             return True
-#         finally:
-#             zf.close()
+def test_model_bundle_from_dirs(
+    # mock_client, requests_mock, fake_project_dir  # noqa: F811
+    launch_client,
+    fake_project_dir,  # noqa: F811
+):
+    # TODO: change this to a valid dirs like in the client_e2e
+    bundle_name = "tmp-bundle-from-dirs"
 
-#     requests_mock.post(
-#         "https://api.scale.com/v1/hosted_inference/model_bundle_upload",
-#         json={
-#             "signedUrl": "s3://my-fake-bucket/path/to/bundle",
-#             "bucket": "my-fake-bucket",
-#             "key": "path/to/bundle",
-#         },
-#     )
-#     requests_mock.put(
-#         "s3://my-fake-bucket/path/to/bundle",
-#         additional_matcher=check_bundle_upload_data,
-#     )
-#     requests_mock.post(
-#         "https://api.scale.com/v1/hosted_inference/model_bundle", json={}
-#     )
+    bundle = launch_client.create_model_bundle_from_dirs(
+        model_bundle_name=bundle_name,
+        base_paths=[
+            os.path.join(fake_project_dir, "project_root/my_module1"),
+            os.path.join(fake_project_dir, "project_root/my_module2"),
+        ],
+        requirements_path=os.path.join(
+            fake_project_dir, "project_root/my_module1/requirements.txt"
+        ),
+        env_params={},
+        load_predict_fn_module_path="a.b.c",
+        load_model_fn_module_path="a.b.c",
+        app_config=None,
+    )
 
-#     mock_client.create_model_bundle_from_dirs(
-#         model_bundle_name="my_test_bundle",
-#         base_paths=[
-#             os.path.join(fake_project_dir, "project_root/my_module1"),
-#             os.path.join(fake_project_dir, "project_root/my_module2"),
-#         ],
-#         requirements_path=os.path.join(
-#             fake_project_dir, "project_root/my_module1/requirements.txt"
-#         ),
-#         env_params={},
-#         load_predict_fn_module_path="a.b.c",
-#         load_model_fn_module_path="a.b.c",
-#         app_config=None,
-#     )
+    assert launch_client.delete_model_bundle(bundle) == "true"
+    logger.info("successfully deleted model bundle {bundle_name}")
 
 
-@pytest.fixture()
+# @pytest.fixture()
 def returns_returns_1(x):
     def returns_1(y):
         return 1
@@ -141,7 +120,7 @@ def returns_returns_1(x):
     return returns_1
 
 
-@pytest.fixture()
+# @pytest.fixture()
 def get_bundle_params():
     # copied from hosted_model_inference/tests/integration/endpoint_builder/client_e2e.py
     env_params = {
@@ -161,7 +140,7 @@ def get_bundle_params():
     return bundle_params
 
 
-@pytest.fixture()
+# @pytest.fixture()
 def get_endpoint_params():
     endpoint_params = {
         # "endpoint_name": "test-endpoint",
@@ -177,77 +156,105 @@ def get_endpoint_params():
     return endpoint_params
 
 
-def test_model_bundle(mock_client, get_bundle_params):
-
-    bundle = mock_client.create_model_bundle(**get_bundle_params)
-    print("successfully created model bundle tmp-bundle")
-
-    # create a bundle with the same name - this should error out
-    with pytest.raises(APIError):
-        bundle = mock_client.create_model_bundle(**get_bundle_params)
-
-    # delete the bundle
-    assert mock_client.delete_model_bundle(bundle) == "true"
-    print("successfully deleted model bundle tmp-bundle")
-
-
-def _test_model_endpoint(mock_client, get_endpoint_params, endpoint_type):
-
-    endpoint_params = get_endpoint_params
-    endpoint_name = f"test-endpoint-{endpoint_type}"
-    endpoint_params["endpoint_name"] = endpoint_name
-    endpoint_params["endpoint_type"] = endpoint_type
-
-    # test-bundle was created beforehand using internal client to bypass the review process
-    bundle = launch.ModelBundle(name="test-bundle")
-
-    print(f"creating model endpoint {endpoint_name} ...")
-    endpoint = mock_client.create_model_endpoint(
-        model_bundle=bundle, **endpoint_params
-    )
-    print(
-        f"successfully created {endpoint_type} model endpoint {endpoint_name}"
-    )
-
-    print("sleeping for 30s")
-    time.sleep(30)
-
-    try:
-        print(f"sending request to model endpoint {endpoint_name}")
-        _make_request(endpoint, endpoint_type)
-        print(f"got response from model endpoint {endpoint_name}")
-
-    finally:
-        # delete the endpoint
-        assert (
-            mock_client.delete_model_endpoint(endpoint.model_endpoint)
-            == "true"
-        )
-        print(f"successfully deleted model endpoint {endpoint_name}")
-
-
-def _make_request(endpoint, endpoint_type):
+def _make_request(endpoint, endpoint_type, expected_output):
     # TODO: make this configurable
-    image_url = "s3://scale-ml-hosted-model-inference/demos/pennfudan/FudanPed00001.png"
+    dummy_input = "s3://scale-ml-hosted-model-inference/demos/pennfudan/FudanPed00001.png"
 
     if endpoint_type == "async":
         future = endpoint.predict(
-            request=EndpointRequest(url=image_url, return_pickled=False)
+            request=EndpointRequest(url=dummy_input, return_pickled=False)
         )
         response = future.get()
     elif endpoint_type == "sync":
         response = endpoint.predict(
-            request=EndpointRequest(url=image_url, return_pickled=False)
+            request=EndpointRequest(url=dummy_input, return_pickled=False)
         )
 
     assert response.status == "SUCCESS"
-    assert response.result == "1"  # question: why it's a string here?
+    assert (
+        response.result == expected_output
+    )  # question: why it's a string here?
 
 
-def test_model_endpoint(mock_client, get_endpoint_params):
-    # _test_model_endpoint(
-    #     mock_client, get_endpoint_params, endpoint_type="async"
-    # )
-    _test_model_endpoint(
-        mock_client, get_endpoint_params, endpoint_type="sync"
+def _edit_endpoint(client, endpoint, endpoint_type):
+    # edit model bundle
+    # test_returns_2 was created beforehand using internal client to bypass the review process
+    returns2_bundle = client.get_model_bundle("test_returns_2")
+    client.edit_model_endpoint(
+        endpoint_name=endpoint.model_endpoint.name,
+        model_bundle=returns2_bundle,
     )
+    _make_request(endpoint, endpoint_type, expected_output="2")
+
+
+def _test_model_endpoint(launch_client, endpoint_type):
+
+    endpoint_params = get_endpoint_params()
+    endpoint_name = f"test-endpoint-{endpoint_type}"
+    endpoint_params["endpoint_name"] = endpoint_name  # TODO: add a timestamp
+    endpoint_params["endpoint_type"] = endpoint_type
+
+    # test_returns_1 was created beforehand using internal client to bypass the review process
+    bundle = launch.ModelBundle(name="test_returns_1")
+
+    logger.info(f"creating model endpoint {endpoint_name} ...")
+    endpoint = launch_client.create_model_endpoint(
+        model_bundle=bundle, **endpoint_params
+    )
+    logger.info(
+        f"successfully created {endpoint_type} model endpoint {endpoint_name}"
+    )
+
+    logger.info("sleeping for 30s")
+    time.sleep(30)
+
+    try:
+        logger.info(f"sending request to model endpoint {endpoint_name}")
+        _make_request(endpoint, endpoint_type, expected_output="1")
+        logger.info(f"got response from model endpoint {endpoint_name}")
+
+        _edit_endpoint(launch_client, endpoint, endpoint_type)
+
+    finally:
+        # delete the endpoint
+        assert (
+            launch_client.delete_model_endpoint(endpoint.model_endpoint)
+            == "true"
+        )
+        logger.info(f"successfully deleted model endpoint {endpoint_name}")
+
+
+def test_model_bundle(launch_client):
+
+    bundle_params = get_bundle_params()
+    bundle = launch_client.create_model_bundle(**bundle_params)
+    logger.info("successfully created model bundle tmp-bundle")
+
+    # create a bundle with the same name - this should error out
+    with pytest.raises(APIError):
+        bundle = launch_client.create_model_bundle(**bundle_params)
+
+    # delete the bundle
+    assert launch_client.delete_model_bundle(bundle) == "true"
+    logger.info("successfully deleted model bundle tmp-bundle")
+
+
+def test_model_endpoint(launch_client):
+    _test_model_endpoint(launch_client, endpoint_type="async")
+    # TODO: currently blocked
+    # _test_model_endpoint(launch_client, endpoint_type="sync")
+
+
+# def test_main(fake_project_dir):
+#     # TODO: add a acript for setting up the test client
+#     client = launch_client()
+#     _test_model_bundle_from_dirs(client, fake_project_dir)
+
+# _test_model_bundle(client)
+
+# _test_model_endpoint(
+#     client, endpoint_type="async"
+# )
+# _test_model_endpoint(
+#     client, get_endpoint_params, endpoint_type="sync"
+# )
