1cfe20f9480708e5040bd2417a0177873a465666
Phil Chen <phil.chen@scale.com>
2023-01-25 10:19:10 -0800

    add autogen client

--- launch/api_client/paths/__init__.py
+++ launch/api_client/paths/__init__.py
@@ -11,10 +11,15 @@ class PathValues(str, enum.Enum):
     READYZ = "/readyz"
     V1_ASYNCTASKS = "/v1/async-tasks"
     V1_ASYNCTASKS_TASK_ID = "/v1/async-tasks/{task_id}"
+    V1_BATCHJOBS = "/v1/batch-jobs"
+    V1_BATCHJOBS_BATCH_JOB_ID = "/v1/batch-jobs/{batch_job_id}"
     V1_MODELBUNDLES = "/v1/model-bundles"
+    V1_MODELBUNDLES_CLONEWITHCHANGES = "/v1/model-bundles/clone-with-changes"
     V1_MODELBUNDLES_LATEST = "/v1/model-bundles/latest"
     V1_MODELBUNDLES_MODEL_BUNDLE_ID = "/v1/model-bundles/{model_bundle_id}"
     V1_MODELENDPOINTS = "/v1/model-endpoints"
+    V1_MODELENDPOINTSAPI = "/v1/model-endpoints-api"
+    V1_MODELENDPOINTSSCHEMA_JSON = "/v1/model-endpoints-schema.json"
     V1_MODELENDPOINTS_MODEL_ENDPOINT_ID = (
         "/v1/model-endpoints/{model_endpoint_id}"
     )
