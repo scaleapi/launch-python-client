06e0c8bdfd8eafa69701ffbd7f79de7797fdc1f5
Adrian Lam <adrian.lam@scale.com>
2023-02-14 10:47:12 -0800

    new send arg

--- launch/cli/endpoints.py
+++ launch/cli/endpoints.py
@@ -1,6 +1,4 @@
-import json
 from pprint import pformat
-from typing import Optional
 
 import click
 import questionary as q
@@ -9,7 +7,7 @@ from rich.table import Table
 from launch.cli.client import init_client
 from launch.cli.console import pretty_print, spinner
 from launch.hooks import PostInferenceHooks
-from launch.model_endpoint import EndpointRequest, ModelEndpoint
+from launch.model_endpoint import ModelEndpoint
 
 
 @click.group("endpoints")
@@ -253,49 +251,3 @@ def edit_endpoint(ctx: click.Context, endpoint_name: str):
         # TODO: Print out a nice error message if the user passes in arguments
         # that fail server-side validation.
         client.edit_model_endpoint(model_endpoint=model_endpoint, **kwargs)
-
-
-@endpoints.command("send")
-@click.argument("endpoint_name")
-@click.option("-r", "--request", help="input request as a json string")
-@click.option("--json-file", help="json file containing request")
-@click.pass_context
-def send(
-    ctx: click.Context,
-    endpoint_name: str,
-    request: Optional[str],
-    json_file: Optional[str],
-):
-    """Sends request to launch endpoint"""
-
-    # Only allowed one kind of input
-    assert (request is not None) ^ (
-        json_file is not None
-    ), "Please supply EITHER --request OR --json-file"
-
-    if request is not None:
-        json_input = json.loads(request)
-    elif json_file is not None:
-        with open(json_file, "rb") as f:
-            json_input = json.load(f)
-
-    client = init_client(ctx)
-
-    model_endpoint = client.get_model_endpoint(endpoint_name)
-    print(f"Sending request to {endpoint_name=} at {ctx.obj.gateway_endpoint}")
-    if model_endpoint.status() is None:
-        raise ValueError(f"Unable to find endpoint {endpoint_name}")
-
-    if model_endpoint.status() != "READY":
-        print(f"Warning: endpoint is not ready get: {model_endpoint.status()}")
-    else:
-        kwargs = {
-            "request": EndpointRequest(args=json_input, return_pickled=False)
-        }
-        if model_endpoint.model_endpoint.endpoint_type == "async":
-            future = model_endpoint.predict(**kwargs)
-            response = future.get()  # blocks until completion
-        else:
-            response = model_endpoint.predict(**kwargs)
-
-        print(response)
