8f6c118fc4de24496f1701729032a9523e6352ac
Ubuntu <ubuntu@i-0c5505e3b813e5baa.us-west-2.compute.internal>
2023-07-24 20:27:34 +0000

    replacing fine_tune_id with id

--- launch/api_client/apis/paths/v1_llm_model_endpoints_model_endpoint_name.py
+++ launch/api_client/apis/paths/v1_llm_model_endpoints_model_endpoint_name.py
@@ -1,6 +1,4 @@
-from launch.api_client.paths.v1_llm_model_endpoints_model_endpoint_name.get import (
-    ApiForget,
-)
+from launch.api_client.paths.v1_llm_model_endpoints_model_endpoint_name.get import ApiForget
 
 
 class V1LlmModelEndpointsModelEndpointName(
