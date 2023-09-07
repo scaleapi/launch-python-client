20a5fc198df7f8694f5ee0605e8716520dc65432
Ian Macleod <ian.macleod@scale.com>
2023-09-07 17:33:14 +0000

    updating autogen

--- launch/api_client/apis/paths/v1_docker_image_batch_job_bundles_docker_image_batch_job_bundle_id.py
+++ launch/api_client/apis/paths/v1_docker_image_batch_job_bundles_docker_image_batch_job_bundle_id.py
@@ -1,6 +1,4 @@
-from launch.api_client.paths.v1_docker_image_batch_job_bundles_docker_image_batch_job_bundle_id.get import (
-    ApiForget,
-)
+from launch.api_client.paths.v1_docker_image_batch_job_bundles_docker_image_batch_job_bundle_id.get import ApiForget
 
 
 class V1DockerImageBatchJobBundlesDockerImageBatchJobBundleId(
