e1706a6cc3e6d9c7bf52c7196f0bfdad19280b29
Anant Marur <anant.marur@scale.com>
2024-05-23 22:06:14 +0000

    modify launch.cli.delete_endpoint to pass endpoint_name directly into client.delete_model_endpoint rather than within a ModelEndpoint object

--- launch/cli/endpoints.py
+++ launch/cli/endpoints.py
@@ -104,8 +104,7 @@ def delete_endpoint(ctx: click.Context, endpoint_name: str):
     client = init_client(ctx)
 
     with spinner(f"Deleting model endpoint '{endpoint_name}'"):
-        endpoint = ModelEndpoint(name=endpoint_name)
-        res = client.delete_model_endpoint(endpoint)
+        res = client.delete_model_endpoint(endpoint_name)
 
     pretty_print(res)
 
